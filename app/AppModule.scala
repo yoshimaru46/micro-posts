313f4a8108bdf8f71fdc6398e893905cd0bbff91
Yoshihiro_Saito <yshr446@gmail.com>
2017-07-02 16:05:33 +0900

    follow

--- app/AppModule.scala
+++ app/AppModule.scala
@@ -5,7 +5,8 @@ class AppModule extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[UserService]).to(classOf[UserServiceImpl])
-    bind(classOf[MicroPostService]).to(classOf[MicroPostServiceImpl]) // 追加
+    bind(classOf[MicroPostService]).to(classOf[MicroPostServiceImpl])
+    bind(classOf[UserFollowService]).to(classOf[UserFollowServiceImpl])
   }
 
 }
